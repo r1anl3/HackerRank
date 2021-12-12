ab19dab618f2b9c93f565c481663caf0cdafb3e8
tuitenrein <levu1409@gmail.com>
2021-12-12 13:27:02 +0700

    m

---
+++ day6.py
@@ -0,0 +1,11 @@
+data = open("D:\Python\day6.txt").read().strip().split(",")
+for i in range(len(data)):
+    data[i] = int(data[i])
+for day in range(1, 81):
+    for i in range(len(data)):
+        if data[i] == 0:
+            data.append(8)
+            data[i] = 6
+        else:
+            data[i] -= 1
+    print("After ", day, " day: ", len(data))
